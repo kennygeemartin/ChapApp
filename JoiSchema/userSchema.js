ff0109fb1a2dd7ce1626e11600e1f53e3e37b3d6
Kenechukwu Agbo <kennygeemartin@gmail.com>
2018-09-13 14:09:37 +0100

    created a joi schema to validate the users details

---
+++ JoiSchema/userSchema.js
@@ -0,0 +1,10 @@
+var Joi = require('joi');
+
+module.exports = Joi.object().keys({
+    username: Joi.string(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    // verified: Joi.boolean(),
+    profilePicture: Joi.any(),
+    profilePictureId: Joi.any(),
+});
